32e2ece7f55d1fa529935ddff08cc8858bd60626
unholydeath785 <ivansonofcoul@gmail.com>
2017-05-22 01:00:52 -0700

    began work on creating syntax tree

--- Main.c
+++ Main.c
@@ -16,18 +16,34 @@
 //ENUMS
 typedef enum {
   TokenType_Pound,
+  TokenType_Class_Selector,
   TokenType_Left_Bracket,
   TokenType_Right_Bracket,
+  TokenType_Equals,
   TokenType_Equals_Equals,
   TokenType_Number,
   TokenType_Semi_Colon,
   TokenType_Colon,
   TokenType_Identifier,
+  TokenType_String,
+  TokenType_Global_Selector,
   TokenType_WhiteSpace,
   TokenType_EOF,
   TokenType_Unknown
 } TokenType;
+
+typedef enum {
+  NodeType_Identifier,
+  NodeType_Global_Selector,
+  NodeType_Selector,
+  NodeType_Class,
+  NodeType_Id,
+  NodeType_Psuedo,
+  NodeType_Equals_Operator,
+  NodeType_String
+} NodeType;
 //END ENUMS
+
 
 //STRUCTS
 typedef struct {
@@ -36,19 +52,21 @@ typedef struct {
 } Token;
 
 typedef struct {
+  Token currentToken;
   char* chars;
   int offset;
   int length;
 } TokenStream;
 
-typedef struct {
+typedef struct _SyntaxNode {
+  NodeType type;
   Token token;
-  struct SyntaxNode* left;
-  struct SyntaxNode* right;
+  struct _SyntaxNode* left;
+  struct _SyntaxNode* right;
 } SyntaxNode;
 
 typedef struct {
-  SyntaxNode root;
+  SyntaxNode* root;
 } SyntaxTree;
 //END STRUCTS
 
@@ -59,7 +77,10 @@ Token tokens[] = {
   {TokenType_Right_Bracket, "]"},
   {TokenType_Semi_Colon, ";"},
   {TokenType_Equals_Equals, "=="},
+  {TokenType_Equals, "="},
   {TokenType_Colon, ":"},
+  {TokenType_Global_Selector, "*"},
+  {TokenType_Class_Selector, "."}
 };
 Token token_eof = {TokenType_EOF, ""};
 //END TOKEN TABLE
@@ -81,33 +102,196 @@ Token readWhiteSpaceToken(TokenStream* stream);
 Token readIndentifierToken(TokenStream* stream);
 Token readNumberToken(TokenStream* stream);
 Token readStringToken(TokenStream* stream, char quoteType);
+Token currentToken(TokenStream* stream);
+void advance(TokenStream* stream);
+int isSelector(Token token);
+void runWhiteSpace(TokenStream* stream);
+SyntaxNode* readIdentifier(TokenStream* stream, SyntaxNode* root);
+SyntaxNode* readString(TokenStream* stream, SyntaxNode* root);
+SyntaxNode* readSelectorType(TokenStream* stream, SyntaxNode* parent);
+SyntaxNode* readCSSSelector(TokenStream* stream, SyntaxNode* child);
+SyntaxNode* readClass(TokenStream* stream, SyntaxNode* node);
+SyntaxNode* readId(TokenStream* stream, SyntaxNode* node);
+SyntaxNode* readPsuedo(TokenStream* stream, SyntaxNode* node);
+SyntaxNode* readSelector(TokenStream* stream, SyntaxNode* root);
+SyntaxNode* readAttribute(TokenStream* stream, SyntaxNode* node);
 //END FUNCTION DECLERATION
+
+char* nodeTypeToString(NodeType type) {
+  switch (type) {
+    case NodeType_Selector:
+      return "NodeType_Selector";
+    case NodeType_Class:
+      return "NodeType_Class";
+    case NodeType_Identifier:
+      return "NodeType_Identifier";
+    case NodeType_Global_Selector:
+      return "NodeType_Global_Selector";
+    case NodeType_Id:
+      return "NodeType_Id";
+    case NodeType_Psuedo:
+      return "NodeType_Psuedo";
+    case NodeType_Equals_Operator:
+      return "NodeType_Equals_Operator";
+    case NodeType_String:
+      return "NodeType_String";
+    default:
+      return "";
+  }
+}
+
+void print(SyntaxNode* node) {
+  if (node != NULL) {
+    printf("%s\n", nodeTypeToString(node -> type));
+    print(node -> left);
+    print(node -> right);
+  }
+}
 
 int main(int argc, char const* argv[]) {
   const char* inputPath = argv[1];
   int isValidInputPath = verifyPath(inputPath, FALSE);
+  SyntaxNode* root = (SyntaxNode*)malloc(sizeof(SyntaxNode));
+
   FILE * file = fopen(inputPath, "r");
   TokenStream* tokenStream = readFile(file);
 
   printWatchingFiles();
 
-  while(TRUE) {
-    // int hasFileUpdated = haveFilesChanged(inputPath);
-    // if (shouldCompile == FALSE) {
-      // printDetectedChanges(inputPath);
-    // }
-    Token token = readToken(tokenStream);
-    if (token.type == TokenType_EOF) {
+  root = readSelector(tokenStream, root);
+  print(root);
+  return 0;
+}
+
+int isSelector(Token token) {
+  return (token.type == TokenType_Pound || token.type == TokenType_Colon ||
+          token.type == TokenType_Class_Selector || token.type == TokenType_Left_Bracket);
+}
+
+void runWhiteSpace(TokenStream* stream) {
+  while(currentToken(stream).type == TokenType_WhiteSpace) {
+    advance(stream);
+  }
+}
+
+SyntaxNode* readSelector(TokenStream* stream, SyntaxNode* root) {
+  int wasWhiteSpace = FALSE;
+  SyntaxNode* node = readIdentifier(stream, root);
+  if (currentToken(stream).type == TokenType_WhiteSpace) {
+    advance(stream);
+  }
+  while (isSelector(currentToken(stream))) {
+    SyntaxNode* parent = readCSSSelector(stream, node);
+    node = parent;
+    while (currentToken(stream).type == TokenType_WhiteSpace) {
+      wasWhiteSpace = TRUE;
+      advance(stream);
+    }
+    if (wasWhiteSpace && currentToken(stream).type == TokenType_Colon) {
       break;
     }
-    if (token.type == TokenType_Unknown) {
-      printf("Unknown: %s\n", token.chars);
-    } else {
-      printf("Token: %s\n", token.chars);
-    }
+  }
+  return node;
+}
+
+SyntaxNode* readCSSSelector(TokenStream* stream, SyntaxNode* child) {
+  SyntaxNode* node = (SyntaxNode*)malloc(sizeof(SyntaxNode));
+  node -> left = child;
+  node -> type = NodeType_Selector;
+  node = readSelectorType(stream, node);
+  advance(stream);
+  return node;
+}
+
+SyntaxNode* readSelectorType(TokenStream* stream, SyntaxNode* parent) {
+  SyntaxNode* node = (SyntaxNode*)malloc(sizeof(SyntaxNode));
+  Token token = currentToken(stream);
+  if (token.type == TokenType_Class_Selector) {
+    advance(stream);
+    readClass(stream, node);
+  }
+  if (token.type == TokenType_Pound) {
+    advance(stream);
+    readId(stream, node);
+  }
+  if (token.type == TokenType_Colon) {
+    advance(stream);
+    readPsuedo(stream, node);
+  }
+  if (token.type == TokenType_Left_Bracket) {
+    advance(stream);
+    readAttribute(stream, node);
+  }
+  parent -> right = node;
+  return parent;
+}
+
+SyntaxNode* readClass(TokenStream* stream, SyntaxNode* node) {
+  Token token = currentToken(stream);
+  if (token.type == TokenType_Identifier) {
+    node -> type = NodeType_Class;
+  }
+  return node;
+}
+
+SyntaxNode* readId(TokenStream* stream, SyntaxNode* node) {
+  Token token = currentToken(stream);
+  if (token.type == TokenType_Identifier) {
+    node -> type = NodeType_Id;
+  }
+  return node;
+}
+
+SyntaxNode* readPsuedo(TokenStream* stream, SyntaxNode* node) {
+  Token token = currentToken(stream);
+  if (token.type == TokenType_Identifier) {
+    node -> type = NodeType_Psuedo;
+  }
+  return node;
+}
+
+SyntaxNode* readAttribute(TokenStream* stream, SyntaxNode* node) {
+  runWhiteSpace(stream);
+  node -> left = readIdentifier(stream, node);
+
+  runWhiteSpace(stream);
+  if (currentToken(stream).type == TokenType_Equals) {
+    node -> type = NodeType_Equals_Operator;
+    advance(stream);
   }
 
-  return 0;
+  runWhiteSpace(stream);
+  if (currentToken(stream).type == TokenType_Identifier) {
+    node-> right = readIdentifier(stream, node);
+  }
+  if (currentToken(stream).type == TokenType_String) {
+    node -> right = readString(stream, node);
+  }
+  return node;
+}
+
+SyntaxNode* readString(TokenStream* stream, SyntaxNode* root) {
+  Token token = currentToken(stream);
+  advance(stream);
+  SyntaxNode* node = (SyntaxNode*)malloc(sizeof(SyntaxNode));
+  if (token.type == TokenType_String) {
+    node -> token = token;
+    node -> type = NodeType_String;
+    return node;
+  }
+  return NULL;
+}
+
+SyntaxNode* readIdentifier(TokenStream* stream, SyntaxNode* root) {
+  Token token = currentToken(stream);
+  advance(stream);
+  SyntaxNode* node = (SyntaxNode*)malloc(sizeof(SyntaxNode));
+  if (token.type == TokenType_Identifier || token.type == TokenType_Global_Selector) {
+    node -> token = token;
+    node -> type = NodeType_Identifier;
+    return node;
+  }
+  return NULL;
 }
 
 TokenStream* readFile(FILE* file) {
@@ -125,6 +309,7 @@ TokenStream* readFile(FILE* file) {
   stream -> chars = chars;
   stream -> offset = 0;
   stream -> length = size;
+  advance(stream);
   return stream;
 }
 
@@ -190,9 +375,18 @@ Token readStringToken(TokenStream* stream, char quoteType) {
     offset++;
   }
   offset++;
-  Token token = createToken(&stream->chars[stream->offset], offset - stream -> offset, TokenType_Number);
+  Token token = createToken(&stream->chars[stream->offset], offset - stream -> offset, TokenType_String);
   stream -> offset = offset;
   return token;
+}
+
+Token currentToken(TokenStream* stream) {
+  return stream -> currentToken;
+}
+
+void advance(TokenStream* stream) {
+  Token token = readToken(stream);
+  stream -> currentToken = token;
 }
 
 //[Token]createToken
